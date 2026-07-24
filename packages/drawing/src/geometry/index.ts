fcbf5f0e5ac2ba8fc98d8bcdf94119b6231b504b
Rishi Vhavle <rishivhavle21@gmail.com>
2026-07-24 09:15:00 +0530

    drop geometry compiler engine and IR types

--- packages/drawing/src/geometry/index.ts
+++
@@ -1,7 +0,0 @@
-export * from "./sceneSpec";
-export * from "./compileTypes";
-export * from "./compileScene";
-export * from "./toTemplate";
-export * from "./inferScene";
-export * from "./postCompileRepair";
-export { solveConstraints } from "./solver";
