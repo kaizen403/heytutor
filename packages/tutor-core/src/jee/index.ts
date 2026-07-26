0ba28ed900988ea0b0b1225d783d4abdf4476477
Rishi Vhavle <rishivhavle21@gmail.com>
2026-07-26 16:00:00 +0530

    drop JEE syllabus modules, scene planner and topic planner

--- packages/tutor-core/src/jee/index.ts
+++
@@ -1,8 +0,0 @@
-export { JEE_NARRATION_LABEL_RULES, type NarrationLabelRule } from "./jeeLabelCues";
-export {
-  JEE_SYLLABUS_TOPICS,
-  buildJeePromptSection,
-  findJeeTopicsByKeyword,
-  type JeeSubject,
-  type JeeTopic,
-} from "./jeeSyllabus";
